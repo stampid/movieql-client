cbf6a7ed8c644ef79f7c275e7fee65ec966e6e32
stampid <vashstampid03@gmail.com>
2019-07-25 15:49:17 +0900

    Creating a Query with variables

--- src/queries.js
+++ src/queries.js
@@ -12,4 +12,20 @@ export const HOME_PAGE = gql`
   }
 `;
 
-export const MOVIE_DETAILS = gql``;
+export const MOVIE_DETAILS = gql`
+  query getMovieDetails($movieId: Int!) {
+    movie(id: $movieId) {
+      medium_cover_image
+      title
+      rating
+      description_intro
+      language
+      genres
+    }
+    suggestions(id: $movieId) {
+      medium_cover_image
+      title
+      rating
+    }
+  }
+`;
